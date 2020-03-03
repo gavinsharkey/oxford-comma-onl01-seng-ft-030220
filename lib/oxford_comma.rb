dfb75b8386209a28dec7bc7e296479aad3248a33
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-03 00:00:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+# There's definitely a better way to do this...
 def oxford_comma(array)
-
+  if array.length == 2
+    array.join(' and ')
+  elsif array.length > 1
+    str = array[0..array.length - 2].join(', ') + ','
+    str << " and #{array[-1]}"
+  else
+    array.join
+  end
 end
